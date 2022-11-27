9d938e5d3a7822a25d8228cd932748d7cb799658
S7ezin <shezinshams2004@gmail.com>
2022-11-27 12:12:19 +0300

    new codes ch-5

---
+++ Chapter 5- Dictionaries/exercise1.py
@@ -0,0 +1,5 @@
+friend ={"first_name":"Muhammed","last_name":"Sufiyan","age":"18","city":"Ajman"}
+print(friend["first_name"])
+print(friend["last_name"])
+print(friend["age"])
+print(friend["city"])
