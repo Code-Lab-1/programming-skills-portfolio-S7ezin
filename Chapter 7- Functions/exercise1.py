699acd8dd67d44d4a48b30e6172f69a7a118e7c5
S7ezin <shezinshams2004@gmail.com>
2022-11-27 14:03:46 +0300

    update codes ch-7

--- Chapter 7- Functions/exercise1.py
+++ Chapter 7- Functions/exercise1.py
@@ -1,3 +1,3 @@
 def display_message(msg):
   print(msg)
-display_message("I am learning how to use function in python")
+display_message("I am learning about function in python")
--- Chapter 7- Functions/exercise5.py
+++ Chapter 7- Functions/exercise5.py
@@ -1,5 +1,5 @@
 def describe_city(city,country="uae"):
   print(city,"is in",country)
+describe_city(city="Ajman")
 describe_city(city="Dubai")
-describe_city(city="Abu dhabi")
-describe_city(city="Tokyo",country="japan")
+describe_city(city="Kerala",country="India")
