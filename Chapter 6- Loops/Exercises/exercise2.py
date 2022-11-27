bf15eb8f6ceec63b4459da33a48172d810abb00e
S7ezin <shezinshams2004@gmail.com>
2022-11-27 12:29:19 +0300

    new codes ch-6

---
+++ Chapter 6- Loops/Exercises/exercise2.py
@@ -0,0 +1,9 @@
+while True:
+  age=int(input("Enter your age: "))
+  age=int(age)
+  if age<3:
+    print("Ticket is free")
+  elif age<13:
+    print("the ticket is $10")
+  else:
+    print("the ticket is $15")
