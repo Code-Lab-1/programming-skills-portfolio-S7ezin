e68f4c5956eaa4e0afbb75ed9c03197ce14af836
S7ezin <shezinshams2004@gmail.com>
2022-11-28 13:46:28 +0300

    update codes ch-5

---
+++ Chapter 5- Dictionaries/exercise9.py
@@ -0,0 +1,7 @@
+Dict = {'Sufy': 18,'Chachu':12,'Tobi':22,'Rock':25}	
+Boys = {'Sufy': 18,'Charlie':12,'Rock':25}
+Girls = {'Tiffany':22}	
+studentX=Boys.copy()
+studentY=Girls.copy()
+print(studentX)
+print(studentY)
