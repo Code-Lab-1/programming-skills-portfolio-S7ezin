f09d65e30e7efec2873f028f66fb697e9defa1df
S7ezin <shezinshams2004@gmail.com>
2022-11-28 13:38:57 +0300

    update codes ch-3

---
+++ Chapter 3- Data Structures/exercise8.py
@@ -0,0 +1,4 @@
+l1 = []
+l2 = [1, 2, "3", 4]
+print(f"List l1: {l1}")
+print(f"List l2: {l2}")
---
+++ Chapter 3- Data Structures/exercise9.py
@@ -0,0 +1,4 @@
+d1 = {}
+d2 = {"Cristiano": {"Age": 37, "Hometown": "Lisbon"}, "Messi": {"Age": 3, "Hometown": "Barcelona"}}
+print(f"Dictionary d1: {d1}")
+print(f"Dictionary d2: {d2}")
