00df76a0896f3682c5ea451ae10e7d96410387b6
S7ezin <shezinshams2004@gmail.com>
2022-11-28 13:54:22 +0300

    update codes ch-7

---
+++ Chapter 7- Functions/exercise9.py
@@ -0,0 +1,8 @@
+# Python program to demonstrate Keyword Arguments
+def student(firstname, lastname):
+	print(firstname, lastname)
+
+
+# Keyword arguments
+student(firstname='Muhammed', lastname='Sufiyan')
+student(lastname='Cristiano', firstname='Ronaldo')
